dbe3e3f7b2c1ec98578656407cfa614ec2ccbba1
juliesigal <90146733+juliesigal@users.noreply.github.com>
2021-11-19 20:13:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 import random
 
-board = [['_','_','_'],['_','_','_'],['_','_','_']]
-current_b = ''
-for i in board:
-    for j in i:
-        current_b += '_'
-
 def present_board(b):
     return f'{b[0:3]}  \n{b[3:6]}  \n{b[6:9]}'
 
@@ -17,9 +11,44 @@ def convert_xo(b,spot,sign):
         else:
             y.append(b[i])
     return ''.join(y)
-b = '_o______o'
-b2 = '_________'
-spot = 3
-sign = 'x'
-current_b= convert_xo(b,spot,sign)
-print(present_board(current_b))
+
+def get_user_sign(b):
+    for i in range(len(b)):
+        s = int(input('please pick a spot in a field'))
+        y = len(b)
+        if s < 0 or s >= y:
+            print('this spot is not exist')
+            continue
+        elif s == i:
+            if b[i] != '_':
+                print('occuped')
+                continue
+    return s
+
+def check_win(b,sign):
+    for i in list(b):
+        if b[0] == sign and b[1] == sign and b[2] == sign\
+            or b[3] == sign and b[4] == sign and b[5] == sign\
+            or b[6] == sign and b[7] == sign and b[8] == sign\
+            or b[0] == sign and b[3] == sign and b[6] == sign\
+            or b[1] == sign and b[4] == sign and b[7] == sign\
+            or b[2] == sign and b[5] == sign and b[8] == sign\
+            or b[0] == sign and b[4] == sign and b[8] == sign\
+            or b[2] == sign and b[4] == sign and b[6] == sign:
+            return f'player {sign} won'
+
+
+
+board = '_________'
+
+for i in range(len(board)):
+    x = get_user_sign(board)
+    current_b = convert_xo(board, x, 'x')
+    board = current_b
+    print(present_board(board))
+    y = get_user_sign(board)
+    current_b = convert_xo(board, y, 'o')
+    board = current_b
+    #print(check_win(board, 'y'))
+    print(present_board(board))
+
