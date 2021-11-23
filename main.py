29c4a74fc438d24e4ca15b55deda836c1a81c4f8
juliesigal <90146733+juliesigal@users.noreply.github.com>
2021-11-23 14:50:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 def present_board(b):      #present the board in 3 different lines to the user
     return f'{b[0:3]}  \n{b[3:6]}  \n{b[6:9]}'
 
-def get_user_sign(b):    #get user's choice, check if it's an integer, if it's legal and free
+#get user's choice, check if it's an integer, if it's legal and free
+def get_user_sign(b):
     while True:
         s = input('please enter number of field')
-        if s.isdigit():    #check if it's an integer
+        if s.isdigit():         #check if it's an integer
             s = int(s)
-            if s < 0 or s > 9:      #if it's legal
+            if s < 0 or s > 9:           #if it's legal
                 print('this spot is not exist')
                 continue
             else:
-                for i in list(b):    # if it's free
+                for i in list(b):       # if it's free
                     if b[s] == '_':
                         return s
                     else:
-                        print("it's occupied, try again")
+                        print(f'position {s} is occupied, please select a different location')
                         break
 
-def convert_xo(b,spot,s):    #replace user's choice in the string
+#replace user's choice in the string
+def convert_xo(b,spot,s):
     y = []
     for i in range(len(b)):
         if i == spot:
@@ -26,7 +28,8 @@ def convert_xo(b,spot,s):    #replace user's choice in the string
             y.append(b[i])
     return ''.join(y)
 
-def check_win(b,s):    #check all the 8 ways to win
+#check all the 8 ways to win
+def check_win(b,s):
     for i in list(b):
         if b[0] == sign and b[1] == sign and b[2] == sign\
             or b[3] == sign and b[4] == sign and b[5] == sign\
@@ -44,8 +47,9 @@ board = '_________'
 print('welcome to the tic-tac-toe game. first player get plays with x and second plays with o. good luck.')
 print(present_board(board))
 
-for i in range(len(board)):    #run on all string fields
-    player_choice = get_user_sign(board)     #check if player 1(x) or 2(o)
+#run on all string fields
+for i in range(len(board)):
+    player_choice = get_user_sign(board)         #check if player 1(x) or 2(o)
     if i % 2 == 0:
         sign = 'x'
     else:
@@ -53,8 +57,8 @@ for i in range(len(board)):    #run on all string fields
     current_b = convert_xo(board, player_choice, sign)
     board = current_b
     print(present_board(board))
-    if check_win(board,sign):                 #check if it's win before the end of the game
+    if check_win(board,sign):                    #check if it's win before the end of the game
         print(check_win(board,sign))
         break
         if i == 8:
-            print("it's a draw")         
+            print("it's a draw") 
